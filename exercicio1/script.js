2036f3afd86ad7fdc2431d99dac61efae29a73a5
Jonathan-Moreira <108627997+Jonathan-Moreira@users.noreply.github.com>
2022-08-23 18:52:20 -0300

    exercicio-funçoes

---
+++ exercicio1/script.js
@@ -0,0 +1,24 @@
+function imprimirOlá(){
+    console.log('Jonathan');
+}
+imprimirOlá()
+
+
+function imprimirTabuada(){
+   let num = 6
+    for ( let i = 1; i <= 10; i++ ) 
+        console.log(num * i);
+}
+imprimirTabuada()
+
+
+
+function imprimirNome(){
+    const nome = 'jonathan'
+    console.log(nome);
+}
+imprimirNome()
+
+// function tabuda() => {
+//     const tabuada = 16
+// }
