85faeb8264a79bbbb7b343ff0ab78f0c2d169043
VictorOgataj8 <victor.ogataj8@gmail.com>
2022-09-14 14:25:18 -0300

    exercicio 1 feito

---
+++ exercicio1/script.js
@@ -0,0 +1,20 @@
+// //a
+// function fraseOi(nome){
+//     console.log(`Ola ${nome}`)
+// }
+// fraseOi(prompt('Insira seu nome'))
+// //b\
+// function tabuada(numero){
+//     // let numero=6
+//     for(let i=1;i<=10;i++)
+//     console.log(i*numero)
+// }
+// tabuada(6)
+const fraseOi= (nome) => console.log(`Ola ${nome}`)
+fraseOi(prompt('Insira seu nome'))
+
+const tabuada= (numero)=> {for(let i = 1;i<=10;i++)
+console.log(i*numero)
+}
+tabuada(prompt('Insira um numero para tabuada'))
+
