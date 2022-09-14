93ab3131aad81bc03e812cc1953c3e6c1c8688ab
camilleferraz <milleagf@gmail.com>
2022-09-14 18:50:15 -0300

    Exercícios feitos

---
+++ exercicio1/script.js
@@ -0,0 +1,31 @@
+// function funcaoOla(nome){
+//     return `Olá, ${nome}!`
+// }
+
+//console.log(funcaoOla(prompt("Qual o seu nome?")))
+
+// function tabuada6(){
+//     for(let i=1;i<=10;i++){
+//         console.log(`6 x ${i} = ${6*i}`)
+//     }
+//    return
+// }
+
+// const tabuada = tabuada6 ()
+
+// console.log(tabuada)
+
+const funcaoOla = (nome) => `Olá, ${nome}!`
+
+console.log(funcaoOla(prompt("Qual o seu nome?")))
+
+const tabuada6 = () => {
+    
+    for(let i=1;i<=10;i++){
+       console.log (`6 X ${i} = ${6*i}`)
+    }
+    
+    
+}
+
+tabuada6()
