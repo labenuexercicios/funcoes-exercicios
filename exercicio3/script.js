16ba5557af302c5297fdc445e26f2cfbe30398e7
VictorOgataj8 <victor.ogataj8@gmail.com>
2022-09-14 15:01:50 -0300

    exercicio 3 feito

---
+++ exercicio3/script.js
@@ -0,0 +1,24 @@
+let numero1= +prompt('Insira um numero')
+let numero2= +prompt('Insira outro numero')
+
+console.log('Primeiro numero: '+ numero1)
+console.log('Segundo numero: '+ numero2)
+
+function numerosAdc(){
+    console.log(`Adicao dos dois numeros: ${numero1+numero2}`)
+}numerosAdc()
+
+function numerosSub(){
+    console.log(`Subtracao dos dois numeros: ${numero1-numero2}`)
+} numerosSub()
+
+function numerosDiv(){
+    console.log(`Divisao dos dois numeros: ${numero1/numero2}`)
+}numerosDiv()
+
+function numerosMult(){
+    console.log(`Multiplicacao dos dois numeros: ${numero1*numero2}`)
+}numerosMult()
+
+
+
