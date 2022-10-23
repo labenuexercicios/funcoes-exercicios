680db13066b348bf735c833ac595b836c4c821f0
fabiodelllima <f.f.fabiodelima@gmail.com>
2022-10-23 04:41:56 -0300

    Exercicio 1 finalizado

---
+++ exercicio1/script.js
@@ -0,0 +1,37 @@
+/* function myName() {
+  console.log(`Olá, Fábio de Lima!`)
+}
+myName()
+
+function tableOfSix() {
+  console.log(6 * 1)
+  console.log(6 * 2)
+  console.log(6 * 3)
+  console.log(6 * 4)
+  console.log(6 * 5)
+  console.log(6 * 6)
+  console.log(6 * 7)
+  console.log(6 * 8)
+  console.log(6 * 9)
+  console.log(6 * 10)
+}
+tableOfSix() */
+
+let myName = () => {
+  console.log(`Olá, Fábio de Lima!`)
+}
+myName()
+
+let tableOfSix = () => {
+  console.log(6 * 1)
+  console.log(6 * 2)
+  console.log(6 * 3)
+  console.log(6 * 4)
+  console.log(6 * 5)
+  console.log(6 * 6)
+  console.log(6 * 7)
+  console.log(6 * 8)
+  console.log(6 * 9)
+  console.log(6 * 10)
+}
+tableOfSix()
