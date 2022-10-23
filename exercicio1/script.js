5b9c0b8e68fb04c08089ebd154514238d2d6affe
WeslleiBrito <wesllei.wbs@gmail.com>
2022-10-23 09:37:29 -0300

    exercicio 1

---
+++ exercicio1/script.js
@@ -0,0 +1,51 @@
+
+// Letra A
+function olaNome(nome){
+    console.log("Olá", nome)
+}
+
+olaNome("Wesllei")
+
+// Letra B
+
+function tabuada(valor){
+    console.log("1 x", valor, "=", 1 * valor)
+    console.log("2 x", valor, "=", 2 * valor)
+    console.log("3 x", valor, "=", 3 * valor)
+    console.log("4 x", valor, "=", 4 * valor)
+    console.log("5 x", valor, "=", 5 * valor)
+    console.log("6 x", valor, "=", 6 * valor)
+    console.log("7 x", valor, "=", 7 * valor)
+    console.log("8 x", valor, "=", 8 * valor)
+    console.log("9 x", valor, "=", 9 * valor)
+    console.log("10 x", valor, "=", 10 * valor)
+}
+
+tabuada(6)
+
+console.log("")
+console.log("Arrow function")
+
+// Letra C parte 1
+const olaNomeArrowfunction = (nome) => {
+    console.log("Olá", nome)
+}
+
+olaNomeArrowfunction("Wesllei Brito")
+
+// Letra C parte 2
+
+const tabuadaArrowFunction = (valor) => {
+    console.log("1 x", valor, "=", 1 * valor)
+    console.log("2 x", valor, "=", 2 * valor)
+    console.log("3 x", valor, "=", 3 * valor)
+    console.log("4 x", valor, "=", 4 * valor)
+    console.log("5 x", valor, "=", 5 * valor)
+    console.log("6 x", valor, "=", 6 * valor)
+    console.log("7 x", valor, "=", 7 * valor)
+    console.log("8 x", valor, "=", 8 * valor)
+    console.log("9 x", valor, "=", 9 * valor)
+    console.log("10 x", valor, "=", 10 * valor)
+}
+
+tabuada(6)
