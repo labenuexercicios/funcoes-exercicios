9a6f755ad89ff11f0c337f8bde0293ef132c7d94
garrcastro <garrcastro@gmail.com>
2022-10-25 12:46:27 -0300

    Exercício 1 incompleto, exercício 2 completo

---
+++ exercicio1/script.js
@@ -0,0 +1,19 @@
+function boasVindas (nome){
+    console.log(`Olá, ${nome}!`)
+}
+boasVindas ("Gabriel")
+
+function tabuada (numero){
+    return numero + 1
+    numero + 2
+    numero + 3
+    numero + 4
+    numero + 5
+    numero + 6
+    numero + 7
+    numero + 8
+    numero + 9
+    console.log(tabuada (numero))
+}
+
+tabuada (1)
