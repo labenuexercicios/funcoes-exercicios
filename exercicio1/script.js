af307073db60dc6c3d6c7496e8a840fbab2458da
garrcastro <garrcastro@gmail.com>
2022-10-25 17:53:21 -0300

    Exercício um feito

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -4,16 +4,35 @@ function boasVindas (nome){
 boasVindas ("Gabriel")
 
 function tabuada (numero){
-    return numero + 1
-    numero + 2
-    numero + 3
-    numero + 4
-    numero + 5
-    numero + 6
-    numero + 7
-    numero + 8
-    numero + 9
-    console.log(tabuada (numero))
+    const zero = numero + 0
+    const um = numero + 1
+    const dois = numero + 2
+    const tres = numero + 3
+    const quatro = numero + 4
+    const cinco = numero + 5
+    const seis = numero + 6
+    const sete = numero + 7
+    const oito = numero + 8
+    const nove = numero + 9
+    console.log(zero, um, dois, tres, quatro, cinco, seis, sete, oito, nove)
+    
 }
+tabuada(1)
+tabuada(2)
+tabuada(3)
+tabuada(4)
+tabuada(5)
+let tabuadaDois =(numero) => {
+    const zero = numero + 0
+    const um = numero + 1
+    const dois = numero + 2
+    const tres = numero + 3
+    const quatro = numero + 4
+    const cinco = numero + 5
+    const seis = numero + 6
+    const sete = numero + 7
+    const oito = numero + 8
+    const nove = numero + 9
+    console.log(zero, um, dois, tres, quatro, cinco, seis, sete, oito, nove)
 
-tabuada (1)
+}
