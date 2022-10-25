13b4fa84cf26ffb0f863b574bc5eb6d360dca51f
Tiago Leite <tiagoleite1405@gmail.com>
2022-10-25 20:42:02 -0300

    Adicionar exercícios de funções

---
+++ exercicio2/script.js
@@ -0,0 +1,23 @@
+//Soma
+const soma = (a, b) => {
+  console.log(a + b);
+};
+soma(5, 5);
+
+//Maior que
+const maiorQue = (a, b) => {
+  console.log(a > b);
+};
+maiorQue(2, 5);
+
+//FizzBuzz
+const fizzBuzz = (n) => {
+  console.log(n % 2 === 0 ? 'Par' : 'Impar');
+};
+fizzBuzz(3);
+
+//inss
+const inss = (n) => {
+  console.log((n / 100) * 90);
+};
+inss(1000);
