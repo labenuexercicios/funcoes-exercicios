4bc0296d2bcbc3b9b1393615eeeaf9b1b286e318
Regiane-Martins <regianegmartins@hotmail.com>
2022-10-25 15:39:47 -0300

    refazendo tabuada primeiro exercício

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -20,7 +20,16 @@ seuNome("Miguel")
 
 
 const tabuada = (numero) => {
-    console.log(numero * 1, numero * 2, numero * 3, numero * 4, numero * 5, numero * 6, numero * 7, numero * 8, numero * 9, numero * 10)
+    console.log(`${numero} X 1 = ${numero *1}`)
+    console.log(`${numero} X 2 = ${numero *2}`)
+    console.log(`${numero} X 3 = ${numero *3}`)
+    console.log(`${numero} X 4 = ${numero *4}`)
+    console.log(`${numero} X 5 = ${numero *5}`)
+    console.log(`${numero} X 6 = ${numero *6}`)
+    console.log(`${numero} X 7 = ${numero *7}`)
+    console.log(`${numero} X 8 = ${numero *8}`)
+    console.log(`${numero} X 9 = ${numero *9}`)
+    console.log(`${numero} X 10 = ${numero *10}`)
 }
 
 tabuada(2)
