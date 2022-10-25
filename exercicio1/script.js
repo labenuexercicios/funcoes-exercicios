a20f9bdf1e9052ea1a917f8a4108d846e4c65c6f
garrcastro <garrcastro@gmail.com>
2022-10-25 18:10:06 -0300

    Exercício um melhorado

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -3,7 +3,7 @@ function boasVindas (nome){
 }
 boasVindas ("Gabriel")
 
-function tabuada (numero){
+/*function tabuada (numero){
     const zero = numero + 0
     const um = numero + 1
     const dois = numero + 2
@@ -21,7 +21,7 @@ tabuada(1)
 tabuada(2)
 tabuada(3)
 tabuada(4)
-tabuada(5)
+tabuada(5)*/
 let tabuadaDois =(numero) => {
     const zero = numero + 0
     const um = numero + 1
@@ -33,6 +33,16 @@ let tabuadaDois =(numero) => {
     const sete = numero + 7
     const oito = numero + 8
     const nove = numero + 9
-    console.log(zero, um, dois, tres, quatro, cinco, seis, sete, oito, nove)
-
+    console.log(`${numero} + 0 =`, zero,)
+    console.log(`${numero} + 1 =`, um,)
+    console.log(`${numero} + 2 =`, dois,)
+    console.log(`${numero} + 3 =`, tres,)
+    console.log(`${numero} + 4 =`, quatro,)
+    console.log(`${numero} + 5 =` ,cinco,)
+    console.log(`${numero} + 6 =` ,seis,)
+    console.log(`${numero} + 7 =`, sete,)
+    console.log(`${numero} + 8 =`, oito,)
+    console.log(`${numero} + 9 =`, nove)
 }
+tabuadaDois(3)
+tabuadaDois(4)
