1fd8d5ff447468d3f217a561e947900c25b9bc8b
Francisco Montalvao <f.montalvao@outlook.com>
2022-10-25 16:05:34 -0300

    resolvido todos

---
+++ exercicio1/script.js
@@ -0,0 +1,36 @@
+function nome (nome){
+    console.log(`Ola ${nome}`)
+}
+nome("Francisco")
+
+// DECLARE UMA FUNCAO QUE FACA A TABUADA DO 6
+
+/* let n1=1
+let n2=2
+let n3=3
+let n4=4
+let n5=5
+let n6=6
+let n7=7
+let n8=8
+let n9=9
+let n10=10
+ */
+
+function tabuada(n2){
+    let n1=6
+    return n1*n2
+}
+console.log(`
+===TABUADA 6===
+===6x1=${tabuada(1)}
+===6x2=${tabuada(2)}
+===6x3=${tabuada(3)}
+===6x4=${tabuada(4)}
+===6x5=${tabuada(5)}
+===6x6=${tabuada(6)}
+===6x7=${tabuada(7)}
+===6x8=${tabuada(8)}
+===6x9=${tabuada(9)}
+===6x10=${tabuada(10)}
+`)
