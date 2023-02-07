dc8f627f2e6f59a52e8ec7629aff010fd1cc03c4
gustaMACHO <henrique.gusta777@gmail.com>
2023-02-07 17:56:21 -0300

    resolucao-exercicios

---
+++ exercicio1/script.js
@@ -0,0 +1,65 @@
+//A)
+function dizerOi(nome){
+    const frase = (`Olá, ${nome}`)
+
+return frase
+}
+
+const ola = dizerOi('gustavo')
+//console.log(ola)
+
+
+//B)
+/*
+function numerosTabuada(numero){
+    console.log(`tabuada de ${numero}:
+    ${numero} x 1 = ${numero * 1}
+    ${numero} x 2 = ${numero * 2}
+    ${numero} x 3 = ${numero * 3}
+    ${numero} x 4 = ${numero * 4}
+    ${numero} x 5 = ${numero * 5}
+    ${numero} x 6 = ${numero * 6}
+    ${numero} x 7 = ${numero * 7}
+    ${numero} x 8 = ${numero * 8}
+    ${numero} x 9 = ${numero * 9}
+    ${numero} x 10 = ${numero * 10}`)
+}
+numerosTabuada(1)
+numerosTabuada(2)
+numerosTabuada(3)
+numerosTabuada(4)
+numerosTabuada(5)
+numerosTabuada(6)
+numerosTabuada(7)
+numerosTabuada(8)
+numerosTabuada(9)
+numerosTabuada(10)
+*/
+
+/*C) 
+const numerosTabuada = (numero) =>{
+    console.log(`tabuada de ${numero}:
+    ${numero} x 1 = ${numero * 1}
+    ${numero} x 2 = ${numero * 2}
+    ${numero} x 3 = ${numero * 3}
+    ${numero} x 4 = ${numero * 4}
+    ${numero} x 5 = ${numero * 5}
+    ${numero} x 6 = ${numero * 6}
+    ${numero} x 7 = ${numero * 7}
+    ${numero} x 8 = ${numero * 8}
+    ${numero} x 9 = ${numero * 9}
+    ${numero} x 10 = ${numero * 10}`)
+}
+
+numerosTabuada(1)
+numerosTabuada(2)
+numerosTabuada(3)
+numerosTabuada(4)
+numerosTabuada(5)
+numerosTabuada(6)
+numerosTabuada(7)
+numerosTabuada(8)
+numerosTabuada(9)
+numerosTabuada(10)
+
+*/
---
+++ exercicio2/script.js
@@ -0,0 +1,27 @@
+//A)
+/*
+function soma(n1, n2) {
+    const soma = n1 + n2
+    const frase = `a soma de ${n1} + ${n2} é ${soma}`
+    
+    return frase 
+
+}
+
+console.log(soma(1, 2))
+console.log (soma(45, 5))
+*/
+
+
+
+/////////////////////////////////////////////////
+
+//B)
+function numeroBooleano(n1, n2) {
+    const compara1 = n1 >= n2
+    console.log(`o primeiro numero é maior ou igual ao segundo? ${compara1}`)
+    
+    return compara1
+}
+
+numeroBooleano(3,2)
