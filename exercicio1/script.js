34da4dbf7f59aa3810dc2d9e75292229d2dc6ab5
GabrielF1008 <gabrielfelipe1009@gmail.com>
2023-03-20 16:24:21 -0300

    exercicio de funcao

---
+++ exercicio1/script.js
@@ -0,0 +1,55 @@
+// // function imprimeNome(nome){
+// //     console.log("Olá", nome)
+// // }
+// const nome = prompt("Digite seu nome")
+//  imprimeNome(nome)
+
+//  function imprimeTabuada(numero){
+//     console.log("1 *" , numero , "=" , 1 * numero)
+//     console.log("2 *" , numero , "=" , 2 * numero)
+//     console.log("3 *" , numero , "=" , 3 * numero)
+//     console.log("4 *" , numero , "=" , 4 * numero)
+//     console.log("5 *" , numero , "=" , 5 * numero)
+//     console.log("6 *" , numero , "=" , 6 * numero)
+//     console.log("7 *" , numero , "=" , 7 * numero)
+//     console.log("8 *" , numero , "=" , 8 * numero)
+//     console.log("9 *" , numero , "=" , 9 * numero)
+//     console.log("10 *" , numero , "=" , 10 * numero)
+//  }
+
+
+
+    const imprimeNome = (nome) =>{
+        console.log("Olá", nome)
+    }
+    const nome = prompt("Digite seu nome")
+    imprimeNome(nome)
+
+    const imprimeTabuada = (numero) => {
+        console.log("1 *" , numero , "=" , 1 * numero)
+        console.log("2 *" , numero , "=" , 2 * numero)
+        console.log("3 *" , numero , "=" , 3 * numero)
+        console.log("4 *" , numero , "=" , 4 * numero)
+        console.log("5 *" , numero , "=" , 5 * numero)
+        console.log("6 *" , numero , "=" , 6 * numero)
+        console.log("7 *" , numero , "=" , 7 * numero)
+        console.log("8 *" , numero , "=" , 8 * numero)
+        console.log("9 *" , numero , "=" , 9 * numero)
+        console.log("10 *" , numero , "=" , 10 * numero)
+    }
+
+    imprimeTabuada(4)
+
+    
+
+
+    
+
+    
+ 
+
+
+
+
+
+
---
+++ exercicio2/script.js
@@ -0,0 +1,32 @@
+// Uma função que recebe 2 números como parâmetros e que imprime a soma deles. Invoque a função e verifique o resultado no console.
+console.log(1)
+
+function somaNumeros(numero1, numero2){
+    console.log(numero1 + numero2)
+}
+
+somaNumeros(2,3)
+
+// Uma função que recebe 2 números e imprime um booleano que informa se o primeiro número é maior ou igual ao segundo.
+
+function maiorMenor(numero3, numero4){
+    console.log(numero3 >= numero4)  // true para primeiro numero maior do que o primeiro e false para primeiro número menor do que o primeiro
+}
+
+maiorMenor(2, 5)
+
+// Uma função que recebe um número e imprime no console um booleano informando se o número é par ou não.
+
+function parOuImpar(numeroTal){
+    console.log(numeroTal % 2 === 0) // true para par e false para ímpar
+}
+
+parOuImpar(10)
+
+// Uma função que recebe um salário bruto e retorna o salário líquido com desconto de 10%, referente ao INSS.
+
+function salarioComDesconto(salario){
+    console.log(salario * 90/100)
+}
+
+salarioComDesconto(1000)
