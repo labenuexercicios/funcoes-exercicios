211b06984285b44cafe411d4f26233ae63a356ba
Lucas Valverde <valverdelucas95@gmail.com>
2023-09-11 14:15:04 -0300

    Respostas do Exercício 1

---
+++ exercicio1/script.js
@@ -0,0 +1,87 @@
+// Parte A 
+
+// // function mostrarNome (nome){
+//     console.log(`Olá, ${nome}!`)
+// }
+
+// mostrarNome("Lucas")
+
+// Parte B
+
+// function tabuada (numero){
+//     tabuada1 = numero * 1
+//     tabuada2 = numero * 2
+//     tabuada3 = numero * 3
+//     tabuada4 = numero * 4
+//     tabuada5 = numero * 5
+//     tabuada6 = numero * 6
+//     tabuada7 = numero * 7
+//     tabuada8 = numero * 8
+//     tabuada9 = numero * 9
+//     tabuada10 = numero * 10
+
+//     console.log(`A tabuada do número ${numero} é:\n
+//     ${numero} * 1 = ${tabuada1} \n
+//     ${numero} * 2 = ${tabuada2} \n
+//     ${numero} * 3 = ${tabuada3} \n
+//     ${numero} * 4 = ${tabuada4} \n
+//     ${numero} * 5 = ${tabuada5} \n
+//     ${numero} * 6 = ${tabuada6} \n
+//     ${numero} * 7 = ${tabuada7} \n
+//     ${numero} * 8 = ${tabuada8} \n
+//     ${numero} * 9 = ${tabuada9} \n
+//     ${numero} * 10 = ${tabuada10}
+//     `)
+// }
+
+// tabuada(1)
+
+// tabuada(2)
+
+// tabuada(3)
+
+// tabuada(4)
+
+///////////////////////////////////////////////////
+
+const mostrarNome = (nome) =>{
+    console.log(`Olá, ${nome}!`)
+}
+
+mostrarNome("Lucas")
+
+///////////////////////////////////////////////////
+
+const montarTabuada = (numero) =>{
+    tabuada1 = numero * 1
+    tabuada2 = numero * 2
+    tabuada3 = numero * 3
+    tabuada4 = numero * 4
+    tabuada5 = numero * 5
+    tabuada6 = numero * 6
+    tabuada7 = numero * 7
+    tabuada8 = numero * 8
+    tabuada9 = numero * 9
+    tabuada10 = numero * 10
+
+    console.log(`A tabuada do número ${numero} é:\n
+    ${numero} * 1 = ${tabuada1} \n
+    ${numero} * 2 = ${tabuada2} \n
+    ${numero} * 3 = ${tabuada3} \n
+    ${numero} * 4 = ${tabuada4} \n
+    ${numero} * 5 = ${tabuada5} \n
+    ${numero} * 6 = ${tabuada6} \n
+    ${numero} * 7 = ${tabuada7} \n
+    ${numero} * 8 = ${tabuada8} \n
+    ${numero} * 9 = ${tabuada9} \n
+    ${numero} * 10 = ${tabuada10}`)
+
+}
+
+montarTabuada(5)
+
+montarTabuada(6)
+
+montarTabuada(7)
+
+montarTabuada(8)
