d7165389f841457e593ebb005c88ea79c81cde20
unknown <fcrispim2012@gmail.com>
2023-09-11 23:24:01 -0300

    exercicio1/script.js

---
+++ exercicio1/script.js
@@ -0,0 +1,20 @@
+// // # Exercício 1
+
+// // a) Declare uma função que imprima `Olá, [SEU NOME]!`. 
+
+// Chame esta função.
+
+// // b) Declare uma função que receba um número como parâmetro e 
+// imprima no console a tabuada de 1 a 10 dele. Chame esta função várias vezes com números diferentes.
+
+// // c) Comente as funções criadas acima, e reescreva-as utilizando 
+// expressões de função, ou __arrow functions__ 
+
+
+function seuNome() {
+    console.log("Olá, Luís Crispim")
+}
+
+seuNome()
+
+
---
+++ exercicio3/script.js
@@ -0,0 +1,53 @@
+// <!-- a) Crie 4 funções. Cada função deve receber dois 
+// parâmetros numéricos e retornar o valor de uma das operações 
+// básicas (soma, subtração, multiplicação e divisão).
+
+// b) Em seguida, fora das funções, receba do usuário dois números e 
+// armazene-os em variáveis.
+
+// c) Invoque cada uma das 4 funções, passando como argumento os dois 
+// números recebidos do usuário.
+
+// d) Por fim, crie variáveis para receber o retorno de cada uma das 
+// funções e imprima o resultado das operações no console. -->
+
+const num1 = +prompt("Insira um número inteiro")
+const num2= +prompt("Insira um número inteiro")
+
+
+function add() {
+    const operacao = num1 + num2
+    return operacao1;
+}
+
+const operacao1 = num1 + num2
+
+function sub() {
+    const operacao = num1 - num2
+    return operacao2;
+}
+const operacao2 = num1 - num2
+
+function mult(num1, num2) {
+    const operacao = num1 * num2
+    return operacao3;
+}
+const operacao3 = num1 * num2
+
+function div(num1, num2) {
+    const operacao = num1 / num2
+    return operacao4;
+}
+const operacao4 = num1 / num2
+
+console.log (operacao1)
+console.log (operacao2)
+console.log (operacao3)
+console.log (operacao4)
+
+
+
+
+
+
+
