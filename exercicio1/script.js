6a13068c359f0d7c5c42a919a214ceb14ac4d4e8
bruhoffman <bruhoffman@gmail.com>
2023-11-13 15:04:31 -0300

    Exercícios Feitos

---
+++ exercicio1/script.js
@@ -0,0 +1,41 @@
+/* function olaMundo(nome) {
+    console.log(`Olá, ${nome}!`);
+}
+
+olaMundo("Bruno");
+
+function tabuada(numero) {
+    console.log(`${numero} * 1 = `, numero * 1);
+    console.log(`${numero} * 2 = `, numero * 2);
+    console.log(`${numero} * 3 = `, numero * 3);
+    console.log(`${numero} * 4 = `, numero * 4);
+    console.log(`${numero} * 5 = `, numero * 5);
+    console.log(`${numero} * 6 = `, numero * 6);
+    console.log(`${numero} * 7 = `, numero * 7);
+    console.log(`${numero} * 8 = `, numero * 8);
+    console.log(`${numero} * 9 = `, numero * 9);
+    console.log(`${numero} * 10 = `, numero * 10);
+}
+
+tabuada(8); */
+
+const olaMundo = function (nome) {
+    console.log(`Olá, ${nome}!`);
+}
+
+olaMundo("Juliano");
+
+const tabuada = (numero) => {
+    console.log(`${numero} * 1 = `, numero * 1);
+    console.log(`${numero} * 2 = `, numero * 2);
+    console.log(`${numero} * 3 = `, numero * 3);
+    console.log(`${numero} * 4 = `, numero * 4);
+    console.log(`${numero} * 5 = `, numero * 5);
+    console.log(`${numero} * 6 = `, numero * 6);
+    console.log(`${numero} * 7 = `, numero * 7);
+    console.log(`${numero} * 8 = `, numero * 8);
+    console.log(`${numero} * 9 = `, numero * 9);
+    console.log(`${numero} * 10 = `, numero * 10);
+}
+
+tabuada(17);
